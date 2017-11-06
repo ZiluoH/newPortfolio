7d1497aecb424e94e4bd4fa8e82d3501a0a04ddd
ZiluoH <ziluoh@gmail.com>
2017-11-05 23:24:59 -0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,3 +10,9 @@ $(document).ready(function(){
 		$(this).toggleClass('animated fadeIn')
 	})
 })
+
+var obj = document.getElementById("navbar");
+    var ot = obj.offsetTop;
+    document.onscroll = function () {
+        var st = document.body.scrollTop || document.documentElement.scrollTop;
+        obj.setAttribute("data-fixed",st >= ot?"fixed":"")}
